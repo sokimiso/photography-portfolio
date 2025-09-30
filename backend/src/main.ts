1ed8f08629b654bb3da486a3075a7ed5b8611b56
Mizo <jozeftrendy@gmail.com>
2025-09-30 16:11:06 +0200

    Updated configs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://sokirka.home',
+    ],
+    credentials: true,
+  });
+
+  //app.setGlobalPrefix('api');
+
   await app.listen(4000);
+  console.log(`Backend running on http://localhost:4000`);
 }
 bootstrap();
